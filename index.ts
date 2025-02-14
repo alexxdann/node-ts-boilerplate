b1d83374ef271225b125d02bf43d8b5d4a977ebb
Alexei Danilin (Alex Dan) / DonKapot <alexdanin.dev@gmail.com>
2025-02-14 20:09:18 +0100

    Update to node 22

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 console.log(
-  "I ask for so little. Just fear me. Love me. Do as I say, and I shall be your slave."
+  "I ask for so little. Just fear me. Love me. Do as I say, and I shall be your slave.\n"
 );
+
+
+const rec = (dir: string) =>
+  console.log("ok");
+
+rec("");
